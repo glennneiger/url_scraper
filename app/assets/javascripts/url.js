865b14328b6f399856e45c60447457770e604745
hazmatzo <maddenwood@gmail.com>
2015-08-31 20:42:49 -0700

    Create initial app to grab a url from a site and display source code.

---
+++ app/assets/javascripts/url.js
@@ -0,0 +1,10 @@
+var highlightTags = function(tag) {
+  _.each($(".tag"), function(el) {
+    if (el.innerText.match(tag) != null && el.innerText.match("<") != null) {
+      $(el).addClass("highlight-yellow");      
+    } else {
+      $(el).removeClass("highlight-yellow");
+    };
+  });
+  console.log("highlighted");
+}
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,34 @@
+require 'open-uri'
+require 'coderay'
+
+class WelcomeController < ApplicationController
+protect_from_forgery
+
+  def index
+    puts "in index"
+  end
+
+  def show
+    parsed_url = Nokogiri::HTML(open(params["url"]))
+    @buttons = make_buttons(parsed_url.xpath("*"))
+    @doc = CodeRay.scan(parsed_url.to_html, :html).html(:line_numbers => :table, :css => :class).html_safe
+  end
+
+  private
+
+  def make_buttons(tag_list)
+    summary = {}
+    summary.default = 0
+    tag_list.each {|node| summary = find_nodes(node, summary)}
+    summary.each {|key, value| "#{key}: #{value}"}
+    summary
+  end
+
+  def find_nodes(node, summary)
+    if node.element?
+        summary[node.name] = summary[node.name] + 1
+    end
+    node.children.each {|node_child| summary = find_nodes(node_child, summary)}
+    summary
+  end
+end
